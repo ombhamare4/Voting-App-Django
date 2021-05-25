f4e64e7bf49f593eeb6c5e1379f36fe9408d40c0
omRubiks <om.rubiks@gmail.com>
2021-05-25 08:01:41 +0530

    Commit Om

--- vote/accounts/forms.py
+++ vote/accounts/forms.py
@@ -5,17 +5,4 @@ class CreatePollForm(ModelForm):
     class Meta:
         model=pollQuetions
         fields=['pollquetion','option1','option2','option3']
-        # widget={
-        #     'pollquetion':forms.CharField(attrs={
-        #         'class':'form-control'
-        #     })
-        #     'option1':forms.CharField(attrs={
-        #         'class':'form-control'
-        #     })
-        #     'option2':forms.CharField(attrs={
-        #         'class':'form-control'
-        #     })
-        #     'option3':forms.CharField(attrs={
-        #         'class':'form-control'
-        #     })
-        # }
+    
